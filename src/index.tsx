a3b1e98c200d99ccf3c17597c92e77c638fa69fc
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-04 17:05:37 +0900

    act 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {ThemeProvider} from 'styled-components';
 import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
