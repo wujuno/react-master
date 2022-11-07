ac924152503b4fccd79535cacc537dd6a7413b69
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-07 14:51:28 +0900

    nested Route

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,14 +1,16 @@
-import { useParams } from "react-router";
+import { useParams, Switch, Route } from "react-router";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Container = styled.div`
     padding: 0px 20px;
     max-width: 480px;
     margin: 0 auto;
 `;
-const Loading = styled.h1`
+const Loader = styled.h1`
     color:${props => props.theme.textColor};
     font-size: 48px;
     text-align: center;
@@ -25,6 +27,28 @@ const Title = styled.h1`
     color:${props => props.theme.accentColor};
     font-size: 48px;
 `
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
 
 interface Params {
     coinId: string;
@@ -106,18 +130,56 @@ function Coin () {
         ).json();
         setInfo(infoData);
         setPriceInfo(priceData);
+        setLoading(false);
       })();
-    }, []);
-    return <Container>
-    <Header>
-        <Title>{state?.name || "Loading..."}</Title>
-    </Header>
-    <div>
-        {loading 
-        ? <Loading>Loading...</Loading>
-        : null};
-    </div>
-    </Container>
-}
-
+    }, [coinId]);
+    return (
+        <Container>
+          <Header>
+            <Title>
+              {state?.name ? state.name : loading ? "Loading..." : info?.name}
+            </Title>
+          </Header>
+          {loading ? (
+            <Loader>Loading...</Loader>
+          ) : (
+            <>
+              <Overview>
+                <OverviewItem>
+                  <span>Rank:</span>
+                  <span>{info?.rank}</span>
+                </OverviewItem>
+                <OverviewItem>
+                  <span>Symbol:</span>
+                  <span>${info?.symbol}</span>
+                </OverviewItem>
+                <OverviewItem>
+                  <span>Open Source:</span>
+                  <span>{info?.open_source ? "Yes" : "No"}</span>
+                </OverviewItem>
+              </Overview>
+              <Description>{info?.description}</Description>
+              <Overview>
+                <OverviewItem>
+                  <span>Total Suply:</span>
+                  <span>{priceInfo?.total_supply}</span>
+                </OverviewItem>
+                <OverviewItem>
+                  <span>Max Supply:</span>
+                  <span>{priceInfo?.max_supply}</span>
+                </OverviewItem>
+              </Overview>
+              <Switch>
+                <Route path={`/${coinId}/price`}>
+                  <Price />
+                </Route>
+                <Route path={`/${coinId}/chart`}>
+                  <Chart />
+                </Route>
+              </Switch>
+            </>
+          )}
+        </Container>
+      );
+    }
 export default Coin;
