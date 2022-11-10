b04d11600eeabe38481587da9bf1a62e54394b9a
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-10 23:56:21 +0900

    toDo list on browser
    using useRecoilState,
    optional interface,

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,23 +1,42 @@
-import {useForm} from "react-hook-form"
+import {useForm} from "react-hook-form";
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 interface IForm {
     toDo: string
 }
+interface IToDo {
+    text: string;
+    id: number;
+    category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+    key: "toDo",
+    default:[],
+})
+
 
 function ToDoList() {
+    const [toDos, setToDos] = useRecoilState(toDoState);
     const {register, handleSubmit, setValue} = useForm<IForm>();
-    const onSubmit = (data:IForm)=>{
-        console.log(`add to do`, data.toDo);
+    const onSubmit = ({toDo}:IForm)=>{
+        setToDos(oldToDos => [{text:toDo, id:Date.now(), category:"TO_DO"},...oldToDos]);
         setValue("toDo", "")
     }
+    console.log(toDos);
     return (
         <div>
+            <h1>To DOs</h1>
             <form onSubmit={handleSubmit(onSubmit)} >
                 <input {...register("toDo", {
                     required:"Please write a To Do",
                     })}  placeholder="Write a to do"/>
                 <button>Add</button>
             </form>
+            <ul>
+              {toDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+
+            </ul>
         </div>
     );
 }
