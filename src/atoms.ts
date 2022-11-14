d5ade43160ea72412e82c3aaba21e08b3775e14c
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-14 11:26:14 +0900

    selector.set()

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,37 +1,19 @@
 import {atom, selector} from "recoil"
 
 
-export enum Categories {
-    "TO_DO" = "TO_DO",
-    "DOING" = "DOING",
-    "DONE" = "DONE",
-  }
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
+});
 
-export interface IToDo {
-    text: string;
-    id: number;
-    category: Categories;
-}
-
-const localToDosJSON = localStorage.getItem("ToDos");
-const localToDos = JSON.parse(localToDosJSON as any);
-
-export const toDoState = atom<IToDo[]>({
-    key: "toDo",
-    default:localToDos,
-})
-
-export const categoryState = atom<Categories>({
-    key:"categoryState",
-    default: Categories.TO_DO
-})
-
-
-export const toDoSelector = selector({
-    key: "toDoSelector",
-    get: ({ get }) => {
-      const toDos = get(toDoState);
-      const category = get(categoryState);
-      return toDos.filter(todo => todo.category === category);
-    },
-  });
+export const hourSelector = selector<number>({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
+});
