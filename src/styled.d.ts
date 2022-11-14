11cf8e0884c40671f425261a12bd98a68c528a88
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-14 13:41:52 +0900

    style and make list to array

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,9 +2,8 @@ import 'styled-components';
 
 declare module 'styled-components' {
     export interface DefaultTheme {
-      bgColor: string,
-      textColor: string,
-      accentColor: string,
-      cardBgColor: string,
+      bgColor: string;
+      boardColor: string;
+      cardColor: string;
       };
 }
