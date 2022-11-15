a8b9a54ef830fcbe2ae2d6c4e19d64cd703797e1
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-15 14:50:38 +0900

    Board Get movement info

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,6 +2,7 @@ import {Droppable} from "react-beautiful-dnd"
 import DragabbleCard from "./DraggleCard"
 import styled from "styled-components";
 
+
 const Wrapper = styled.div`
   width: 300px;
   padding: 20px 10px;
@@ -9,6 +10,8 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -17,6 +20,17 @@ const Title = styled.h2`
   margin-bottom: 10px;
   font-size: 18px;
 `;
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${props => props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+`;
+
+interface IAreaProps {
+  isDraggingFromThis: boolean;
+  isDraggingOver: boolean;
+}
 
 interface IBoardProps {
     toDos: string[];
@@ -28,12 +42,16 @@ function Board ({toDos, boardId}:IBoardProps) {
         <Wrapper>
             <Title>{boardId}</Title>
         <Droppable droppableId={boardId}>
-            {(magic) => (
-              <div ref={magic.innerRef} {...magic.droppableProps}>
+            {(magic, info) => (
+              <Area 
+              isDraggingOver={info.isDraggingOver}
+              isDraggingFromThis={Boolean(info.draggingFromThisWith)} 
+              ref={magic.innerRef} 
+              {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
                   <DragabbleCard key={toDo} index={index} toDo={toDo} />))}
 				{magic.placeholder}
-                </div>
+                </Area>
 			  )}
 		</Droppable>
         </Wrapper>
